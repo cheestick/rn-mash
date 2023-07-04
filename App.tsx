b85291bf25c40bcc133242f9fd44536f27a88f34
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2023-07-04 20:54:00 +0300

    flat list

--- App.tsx
+++ App.tsx
@@ -1,50 +1,58 @@
 import { useState } from 'react';
-import { RefreshControl, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { FlatList, RefreshControl, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { dataList, titleList } from './mock';
 
-const data = [
-  {key:1, item: 'item 1'}, 
-  {key:2, item: 'item 2'}, 
-  {key:3, item: 'item 3'}, 
-  {key:4, item: 'item 4'}, 
-  {key:5, item: 'item 5'}, 
-  {key:6, item: 'item 6'}, 
-  {key:7, item: 'item 7'}, 
-  {key:8, item: 'item 8'}, 
-  {key:9, item: 'item 9'}, 
-  {key:10, item: 'item 10'}, 
-  {key:11, item: 'item 11'}, 
-  {key:12, item: 'item 12'}, 
-]
 
 export default function App() {
-  const [items, setItems] = useState(data)
+  const [items, setItems] = useState(dataList)
   const [refreshing, setRefreshing] = useState(false)
 
   const onRefresh = () => {
     setRefreshing(true)
-    const newItemCounter = items.length + 1
+    const newItemCounter = (items.length + 1).toString()
     setTimeout(() => {
-      setItems([...items, {key: newItemCounter, item: `item ${newItemCounter}`}])
+      setItems([...items, {id: newItemCounter, text: `item ${newItemCounter}`}])
       setRefreshing(false)
     }, 1000)
   }
 
   return (
     <View style={sx.container}>
-      <ScrollView horizontal={false} refreshControl={<RefreshControl 
+
+      <FlatList
+        data={items}
+        // numColumns={2}
+        horizontal={false}
+        // inverted
+        keyExtractor={(item, index) => item.id.toString()}
+        renderItem={({item}) => (
+          <View style={sx.item} >
+              <Text  style={sx.text}>{item.text}</Text>
+          </View>
+        )}
+        refreshControl={<RefreshControl 
+          refreshing={refreshing} 
+          onRefresh={onRefresh}
+          tintColor={'#ff00ff45'}
+          />}
+      />
+
+      {/* <ScrollView 
+        horizontal={false} 
+        refreshControl={<RefreshControl 
         refreshing={refreshing} 
         onRefresh={onRefresh}
         colors={['#ff00ff']}
         />}>
 
         {items.map(it =>  (
-          <View style={sx.item} key={it.key}>
-              <Text  style={sx.text}>{it.item}</Text>
+          <View style={sx.item} key={it.id}>
+              <Text  style={sx.text}>{it.text}</Text>
           </View>
         )
         )}
       
-        </ScrollView>
+        </ScrollView> */}
     </View>
   );
 }
@@ -63,7 +71,9 @@ const sx = StyleSheet.create({
     marginVertical: 10,
     marginHorizontal: 20,
     paddingVertical: 20,
+    paddingHorizontal: 25,
     backgroundColor: '#12af3d54',
+    borderRadius: 15,
   },
   text: {
     fontSize: 24,
---
+++ mock/index.ts
@@ -0,0 +1,33 @@
+export const dataList = [
+  {id:'1', text: 'item 1'}, 
+  {id:'2', text: 'item 2'}, 
+  {id:'3', text: 'item 3'}, 
+  {id:'4', text: 'item 4'}, 
+  {id:'5', text: 'item 5'}, 
+  {id:'6', text: 'item 6'}, 
+  {id:'7', text: 'item 7'}, 
+  {id:'8', text: 'item 8'}, 
+  {id:'9', text: 'item 9'}, 
+  {id:'10', text: 'item 10'}, 
+  {id:'11', text: 'item 11'}, 
+  {id:'12', text: 'item 12'}, 
+]
+
+export const titleList = [
+  {
+    title: 'Title 1',
+    data: ['Item 101', 'Item 102', 'Item 103'],
+  },
+  {
+    title: 'Title 2',
+    data: ['Item 201', 'Item 202', 'Item 203'],
+  },
+  {
+    title: 'Title 3',
+    data: ['Item 301', 'Item 302', 'Item 303'],
+  },
+  {
+    title: 'Title 4',
+    data: ['Item 401', 'Item 402', 'Item 403'],
+  },
+]
